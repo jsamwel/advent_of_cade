005bcb6195575b6bb01c019ba57a8b8190136204
jsamwel <7791315+jsamwel@users.noreply.github.com>
2022-12-13 13:36:51 +0100

    Upload day 6

---
+++ day6/part1.py
@@ -0,0 +1,15 @@
+with open('puzzle_input.txt') as file:
+    puzzle_input = file.read()
+    
+start_indexes = []
+
+for index in range(len(puzzle_input[:2000])):
+    sequence = puzzle_input[index-4:index]
+    
+    if len(sequence) == 0:
+        continue
+    
+    if len(set(sequence)) == len(sequence):
+        start_indexes.append(index)
+
+print(puzzle_input[start_indexes[0]-4:start_indexes[0]], start_indexes[0])
